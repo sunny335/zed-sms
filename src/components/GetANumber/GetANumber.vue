243a4e6a5ad9061ac554eadce552894d1a7ad073
sunnyicodelabs <sunny@icodelabs.co>
2023-09-19 00:05:55 +0600

    added home page new section

---
+++ src/components/GetANumber/GetANumber.vue
@@ -0,0 +1,116 @@
+
+<script setup>
+import PhilipinsImg from '../../assets/img/Philipins.png';
+</script>
+
+<template>
+    <div>
+        <h1 class="text-neutral-800 text-center text-[32px] font-medium font-['Poppins'] leading-[22px]">To Get A Number
+        </h1>
+        <p class="text-neutral-800 text-center text-base font-light font-['Poppins'] leading-[11px] mt-[20px]">for any kind
+            of services</p>
+    </div>
+    <div class="flex flex-wrap mt-[30px]">
+        <div class="w-full">
+            <ul
+                class="flex mb-0 list-none flex-row max-w-[450px] mx-auto rounded-2xl border border-blue-600 overflow-hidden	 h-[58px]">
+                <li class="-mb-px last:mr-0 flex-auto text-center cursor-pointer">
+                    <a class="w-[150px] flex items-center justify-center text-center text-lg font-light font-['Poppins'] leading-[13px] px-5 pt-2 pb-3 h-[58px] "
+                        v-on:click="toggleTabs(1)"
+                        v-bind:class="{ 'text-neutral-800 bg-white': openTab !== 1, 'text-white bg-blue-600': openTab === 1 }">
+                        Shared
+                    </a>
+                </li>
+                <li class="-mb-px last:mr-0 flex-auto text-center cursor-pointer">
+                    <a class="w-[150px] flex items-center justify-center text-center text-lg font-light font-['Poppins'] leading-[13px] px-5 pt-2 pb-3 h-[58px] "
+                        v-on:click="toggleTabs(2)"
+                        v-bind:class="{ 'text-neutral-800 bg-white': openTab !== 2, 'text-white bg-blue-600': openTab === 2 }">
+                        Private
+                    </a>
+                </li>
+                <li class="-mb-px last:mr-0 flex-auto text-center cursor-pointer">
+                    <a class="w-[150px] flex items-center justify-center text-center text-lg font-light font-['Poppins'] leading-[13px] px-5 pt-2 pb-3 h-[58px] "
+                        v-on:click="toggleTabs(3)"
+                        v-bind:class="{ 'text-neutral-800 bg-white': openTab !== 3, 'text-white bg-blue-600': openTab === 3 }">
+                        Temporary
+                    </a>
+                </li>
+            </ul>
+            <div
+                class="w-full mt-[60px] px-[30px] pt-[34px] pb-[30px] rounded-[36px] border border-zinc-600 flex-col justify-start items-center gap-[30px] inline-flex">
+                <div>
+
+                    <div class="tab-content tab-space">
+                        <div class="flex items-center gap-[20px] justify-center border-b border-zinc-600 w-max mx-auto pb-[26px]">
+                            <div class="flex gap-[6px] items-center">
+                                <div class="text-center text-zinc-600 text-sm font-normal font-['Poppins']">Country:
+                                </div>
+                                <div class="flex gap-[6px] items-center">
+                                    <img :src="PhilipinsImg" alt="Image Description" />
+
+                                    <div class="text-center text-zinc-600 text-sm font-light font-['Poppins']">South
+                                        Africa</div>
+                                </div>
+                            </div>
+                            <div class="flex gap-[6px] items-center">
+                                <div class="text-center text-zinc-600 text-sm font-normal font-['Poppins']">Service:
+                                </div>
+                                <div class="flex gap-[6px] items-center">
+                                    <img :src="PhilipinsImg" alt="Image Description" />
+                                    <div class="text-center text-zinc-600 text-sm font-light font-['Poppins']">
+                                        Instagram</div>
+                                </div>
+                            </div>
+                            <div class="flex gap-[6px] items-center">
+                                <div class="text-center text-zinc-600 text-sm font-normal font-['Poppins']">Number:
+                                </div>
+                                <div class="text-zinc-600 text-sm font-light font-['Poppins']">+880 12321 2***5232</div>
+                            </div>
+                            <div class="flex gap-[6px] items-center">
+                                <div class="text-center text-zinc-600 text-sm font-normal font-['Poppins']">Rent:
+                                </div>
+                                <div class="text-zinc-600 text-sm font-light font-['Poppins']">1 Week</div>
+                            </div>
+                            <div class="flex gap-[6px] items-center">
+                                <div class="text-center text-zinc-600 text-sm font-normal font-['Poppins']">Cost:
+                                </div>
+                                <div class="text-zinc-600 text-sm font-light font-['Poppins']">$00.00</div>
+                            </div>
+                        </div>
+                        <div v-bind:class="{ 'hidden': openTab !== 1, 'block': openTab === 1 }">
+                            <div class="mt-[30px]">
+                              tab1
+                            </div>
+                        </div>
+                        <div v-bind:class="{ 'hidden': openTab !== 2, 'block': openTab === 2 }">
+                            <div  class="mt-[30px]">
+                            tab2
+                            </div>
+                        </div>
+                        <div v-bind:class="{ 'hidden': openTab !== 3, 'block': openTab === 3 }">
+                            <div  class="mt-[30px]">
+                              tab3
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "pink-tabs",
+    data() {
+        return {
+            openTab: 1
+        }
+    },
+    methods: {
+        toggleTabs: function (tabNumber) {
+            this.openTab = tabNumber
+        }
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,18 @@
 <script setup>
+import GetANumber from '../components/GetANumber/GetANumber.vue';
+
+
 </script>
 
 <template>
-  <main>
-    <div>
-
-    </div>
-
+  <main class="pb-[103px]">
     <div class="px-4 pt-21 mx-auto sm:max-w-xl md:max-w-full lg:max-w-[1192px] md:px-24 lg:px-8 lg:pt-[84px]">
-      <div class="flex flex-col items-center justify-between w-full mb-10 lg:flex-row">
+      <div class="flex flex-col items-center justify-between w-full mb-[157px] lg:flex-row">
         <div class="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
           <div class="max-w-xl mb-6">
-            <h3 class="w-[387px] text-gray-900 text-[42px] font-medium font-['Poppins'] leading-[50px]">Send & Receive
+            <h3 class="max-w-[387px] text-gray-900 text-[42px] font-medium font-['Poppins'] leading-[50px]">Send & Receive
               Message Online </h3>
-            <p class="w-[387px] mt-[30px] text-zinc-600 text-base font-normal font-['Poppins'] leading-relaxed">Powerful,
+            <p class="max-w-[387px] mt-[30px] text-zinc-600 text-base font-normal font-['Poppins'] leading-relaxed">Powerful,
               self-serve product easy to use for easily communicate with our virtual sim cards, sign up us or see the demo
             </p>
           </div>
@@ -548,6 +547,9 @@
 
       </div>
     </div>
+    <GetANumber />
   </div>
 
+
 </main></template>
+
