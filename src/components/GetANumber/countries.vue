444aecaac17092e7b9288f9065735b37d3b5044b
sunnyicodelabs <sunny@icodelabs.co>
2023-09-19 10:14:56 +0600

    added get a number component

---
+++ src/components/GetANumber/countries.vue
@@ -0,0 +1,11 @@
+// countries.js
+
+export const countries = [
+  { name: 'United States', flag: 'united-states.png' },
+  { name: 'Canada', flag: 'canada.png' },
+  { name: 'United Kingdom', flag: 'united-kingdom.png' },
+  { name: 'France', flag: 'france.png' },
+  { name: 'Germany', flag: 'germany.png' },
+  { name: 'Spain', flag: 'spain.png' },
+  // Add more countries and flags as needed
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: [ './src/**/*.html', './src/**/*.vue', './src/**/*.jsx', ],
   darkMode: 'class', // Enable dark mode using class
@@ -23,8 +25,29 @@ module.exports = {
     fontFamily:{
       Poppins:["Poppins","sans-sarif"],
       Inter:["Inter","sans-sarif"]
-    }
+    },
+    
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+          '.scrollbar': {
+              overflowY: 'auto',
+              scrollbarColor: `#DDE2E5`,
+              scrollbarWidth: 'thin',
+          },
+          '.scrollbar::-webkit-scrollbar': {
+              height: '2px',
+              width: '5px',
+          },
+          '.scrollbar::-webkit-scrollbar-thumb': {
+              backgroundColor: "#DDE2E5",
+          },
+          '.scrollbar::-webkit-scrollbar-track-piece': {
+              backgroundColor: "#fff",
+          },
+      });
+  }),
+  ],
 }
 
